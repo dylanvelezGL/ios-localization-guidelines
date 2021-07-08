6c76e1a6ad28589c9fa511e050b5056d96bfc18e
Dylan Velez <dylan.velez@gorillalogic.com>
2021-07-08 13:59:20 -0500

    Initial Commit

---
+++ FoodApp/FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppApp.swift
+//  FoodApp
+//
+//  Created by Dylan Vélez on 8/07/21.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
