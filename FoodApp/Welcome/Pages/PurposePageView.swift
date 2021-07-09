320706163e8e5d97af44866eb0028a81ff70d9e6
DylanVelezCode <dylanvelez.code@gmail.com>
2021-07-09 12:47:42 -0500

    Added AppIcon and  Finishing Welcome Screen structure

--- FoodApp/Welcome/Pages/PurposePageView.swift
+++ FoodApp/Welcome/Pages/PurposePageView.swift
@@ -10,11 +10,26 @@ import SwiftUI
 struct PurposePageView: View {
     var body: some View {
         VStack {
+            Spacer()
+                .frame(height: 40)
             Text("Welcome to FoodApp")
-            Image(systemName: "mail")
-            Text("This app was made with the purpose of teaching some localization strategies")
-            Text("If you want to know more please look for the blog post here")
+                .font(.largeTitle)
+            Spacer()
+                .frame(height: 40)
+            Image("groceries")
+            Spacer()
+                .frame(height: 40)
+            Text("This app was made with the purpose of teaching some localization strategies. \nIt includes localization for English, Spanish and Arabic and you can switch between the locales at any time in the settings screen.")
+                .font(.footnote)
+                .padding()
+            Spacer()
+                .frame(height: 20)
+            Text("For more info please visit this blog post here")
+                .font(.headline)
+                .padding()
+            Spacer()
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
